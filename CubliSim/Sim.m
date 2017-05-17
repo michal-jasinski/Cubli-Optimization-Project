e284ea55768fe6207d0649a2b6f0d802d4adcdcc
michał jasiński <michal.jasinski72@gmail.com>
2017-05-17 11:57:08 +0200

    Full simulation without optimization
    Only non-linear controller without optimization

--- CubliSim/Sim.m
+++ CubliSim/Sim.m
@@ -55,14 +55,17 @@ pww_0=[0;0;0];
 pwh_0=[0;0;0];
 
 Q_0 = 0;
-
+global control;
+control=[0 0 0];
 x0 = [g_0;pwh_0;pww_0;PIK_0;Q_0];
+T = zeros(1,2000);
 sample_time = 0.005;
-time = 2;
+time = length(T)*sample_time;
 tspan = 0:sample_time:time;
 
 % [t,x] = ode45(@(t,x)rhs(t,x,Theta_0_ht,m,alpha,beta,gamma,delta),tspan,x0);
-[t,x] = rk4(@rhs,x0,time,sample_time,Theta_0_ht,m);
+
+[t,x] = rk4(@rhs,x0,time,T,Theta_0_ht,m,alpha,beta,gamma,delta);
 
 % Extract data
 PIK=x(:,10:13);
