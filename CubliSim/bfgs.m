60f109d0cddf2c2521f80dc06d57f1068a3916f8
michał jasiński <michal.jasinski72@gmail.com>
2017-05-21 10:25:29 +0200

    Optymalizacja wersja testowa - minimalne poprawki w stosunku do poprzedniej wersji. Została przeprowadzona symulacja, która po godzinie wyliczyła minimum (nie jest ono super, ale jest). Wynik został zapisany jako workspace local_minimum.mat.

--- CubliSim/bfgs.m
+++ CubliSim/bfgs.m
@@ -1,12 +1,15 @@
 function [ Q, gradient] = bfgs(u)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-
+    global Q_hist;
+    global iter;
     init;
     disp(u)
     [t,x,psi,gradient] = rk4(@rhs,@rhs_sprzezone,x0,time,sample_time,Theta_0_ht,m,u);
-
+    
     Q = x(end,end)
-
+    Q_hist(iter) = Q;
+    iter = iter + 1
+    
 end
 
