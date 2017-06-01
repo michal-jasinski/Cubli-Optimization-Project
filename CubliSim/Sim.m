f22162cc8274cd05b49fc269965bd9493bb93b25
michał jasiński <michal.jasinski72@gmail.com>
2017-06-01 11:36:40 +0200

    Dodana eskansja do bfgs. Przetestowane kilka wartości parametrów kroku, ekspansji i kontrakcji. Wyniki zapisane w komentarzu.

--- CubliSim/Sim.m
+++ CubliSim/Sim.m
@@ -22,76 +22,76 @@ maxIter = 500;
 %                         'Display','iter-detailed');
 % retval = fminunc(@get_quality_indicator,u0,options)
 
-retval = bfgs(u0,epsilon,lb,ub,maxIter)
+% retval = bfgs(u0,epsilon,lb,ub,maxIter)
 
  
-% retval = [
-%      6.934161042971873e-02
-%      2.563401714063781e-02
-%     -9.941398766136766e-02
-%      2.234805925923642e-03
-%      1.162939769305259e-03
-%     -3.544536592805359e-03
-%     -6.408200313467240e-03
-%     -2.581676789982574e-03
-%      9.350003109377073e-03
-%     -3.970712885263781e-03
-%     -1.793001106040176e-03
-%      5.987394154127943e-03
-%     -1.196767260802018e-03
-%     -5.829287977509501e-04
-%      1.840861075819404e-03];
-%  
-% % simulation
-% [t,x,psi,gradient] = rk4(@rhs,@rhs_sprzezone,x0,time,sample_time,Theta_0_ht,m,retval,nodes);
-% 
-% % Extract data
-% PIK=x(:,10:13);
-% pwh=x(:,4:6);
-% pww=x(:,7:9);
-% 
-% % express m-vector in inertial coordinate frame
-% I_m=zeros(length(t),3);
-% I_pwh=zeros(length(t),3);
-% 
-% for k=1:length(t)
-%     A_IKtmp=eye(3)+2*PIK(k,1)*Skew(PIK(k,2:4))+2*Skew(PIK(k,2:4))^2;
-%     I_m(k,:)=(A_IKtmp*m)';
-%     I_pwh(k,:)=(A_IKtmp*(pwh(k,:)'))';
-% end
-% 
-% % calculate inclination angle
-% figure(1)
-% hold all;
-% plot(t,acos(I_m(:,1)/norm(m))/pi*180);
+retval = [
+     6.988899070570910e-02
+     2.585493712809231e-02
+    -1.002168478015027e-01
+     1.220755487231957e-03
+     7.864959335636749e-04
+    -2.092179854122555e-03
+    -6.806906524662713e-03
+    -2.757340255207410e-03
+     9.947610045352438e-03
+    -3.991471127606128e-03
+    -1.820388565884470e-03
+     6.036395939753204e-03
+    -1.168581773759151e-03
+    -5.790219549295714e-04
+     1.807658730308782e-03];
+
+% simulation
+[t,x,psi,gradient] = rk4(@rhs,@rhs_sprzezone,x0,time,sample_time,Theta_0_ht,m,retval,nodes);
+
+% Extract data
+PIK=x(:,10:13);
+pwh=x(:,4:6);
+pww=x(:,7:9);
+
+% express m-vector in inertial coordinate frame
+I_m=zeros(length(t),3);
+I_pwh=zeros(length(t),3);
+
+for k=1:length(t)
+    A_IKtmp=eye(3)+2*PIK(k,1)*Skew(PIK(k,2:4))+2*Skew(PIK(k,2:4))^2;
+    I_m(k,:)=(A_IKtmp*m)';
+    I_pwh(k,:)=(A_IKtmp*(pwh(k,:)'))';
+end
+
+% calculate inclination angle
+figure(1)
+hold all;
+plot(t,acos(I_m(:,1)/norm(m))/pi*180);
+xlabel('time [s]')
+ylabel('inclination angle [deg]')
+figure(2)
+hold all;
+plot(t,acos(I_m(:,2)/norm(m))/pi*180);
+xlabel('time [s]')
+ylabel('inclination angle [deg]')
+figure(3)
+hold all;
+plot(t,acos(I_m(:,3)/norm(m))/pi*180);
+xlabel('time [s]')
+ylabel('inclination angle [deg]')
+% body angulr velocity
+wh=(pwh-pww)*Theta_0_ht^-1;
+
+% wheel velocity
+ww=pww*Theta_w^-1-wh;
+
+figure(4)
+plot(t,wh)
+ylabel('\omega_h [rad/s]')
+xlabel('time [s]')
+
+% figure
+% plot(t,ww/(2*pi)*60)
+% ylabel('\omega_w [rpm]')
 % xlabel('time [s]')
-% ylabel('inclination angle [deg]')
-% figure(2)
-% hold all;
-% plot(t,acos(I_m(:,2)/norm(m))/pi*180);
-% xlabel('time [s]')
-% ylabel('inclination angle [deg]')
-% figure(3)
-% hold all;
-% plot(t,acos(I_m(:,3)/norm(m))/pi*180);
-% xlabel('time [s]')
-% ylabel('inclination angle [deg]')
-% % body angulr velocity
-% wh=(pwh-pww)*Theta_0_ht^-1;
-% 
-% % wheel velocity
-% ww=pww*Theta_w^-1-wh;
-% 
-% figure(4)
-% plot(t,wh)
-% ylabel('\omega_h [rad/s]')
-% xlabel('time [s]')
-% 
-% % figure
-% % plot(t,ww/(2*pi)*60)
-% % ylabel('\omega_w [rpm]')
-% % xlabel('time [s]')
-% 
-% quality_indicator = x(:,14);
-% figure(5);
-% plot(t,quality_indicator);
+
+quality_indicator = x(:,14);
+figure(5);
+plot(t,quality_indicator);
--- CubliSim/bfgs.m
+++ CubliSim/bfgs.m
@@ -1,12 +1,22 @@
 function [ optimal_control ] = bfgs(u,epsilon,lb,ub,maxIter)
     
     % brief - zak³adamy, ¿e u jest wektorem o jednej kolumnie
+	% Iteration - 183  Q = 0.0000986938  norm = 0.0003999640 - step 0.1 for first 50 iteration, expansion 5, contraction 0.5
+    % Iteration - 66  Q = 0.0000981280  norm = 0.0003991874  - step 0.1 for first 100 iterations, expansion 5, contraction 0.5
+    % Iteration - 102  Q = 0.0000937457  norm = 0.0002959723 - step 0.1 for first 100 iterations, expansion 5, contraction 0.5
+    % step 0.2 for first 100 iterations, expansion 5, contraction 0.5 - worse results than with the previous settings
+    % Iteration - 104  Q = 0.0000939161  norm = 0.0002982066 - step 0.1 for first 100 iterations, expansion 3, contraction 0.5
+    % Iteration - 448  Q = 0.0000939920  norm = 0.0002999514 - step 0.1 for first 100 iterations, expansion 2, contraction 0.5
+    % Iteration - 354  Q = 0.0000899344  norm = 0.0001999277 - step 0.1 for first 150 iterations, expansion 3, contraction 0.5
+    % Iteration - 191  Q = 0.0000894085  norm = 0.0001916188 - step 0.1 for first 200 iterations, expansion 3, contraction 0.5
+    % ostatnie dzia³a najlepiej - chyba
     % initialize parameters
     init;
     
     % reneval parameter
     Reneval = 1;
-    contraction = 0.1;
+    contraction = 0.5;
+    expansion = 3;
     error = 0;
     optimal_control = zeros(size(u));
     
@@ -45,7 +55,7 @@ function [ optimal_control ] = bfgs(u,epsilon,lb,ub,maxIter)
         end
         
         % line search
-        step = 2;
+        step = 0.1;
         for j = 1 : 100
             u_temp = u;
             % get new control with saturation
@@ -65,15 +75,46 @@ function [ optimal_control ] = bfgs(u,epsilon,lb,ub,maxIter)
             
             % check stop conditions
             if Q_temp < Q
-                Q = Q_temp;
-                Reneval = 0;
-                s = u_temp - u;
-                r = gradient_temp - gradient;
-                u = u_temp;
-                gradient = gradient_temp;
+                if i < 200
+                    % expansion
+                    while Q_temp < Q
+                        
+                        Q = Q_temp;
+                        Reneval = 0;
+                        s = u_temp - u;
+                        r = gradient_temp - gradient;
+                        u = u_temp;
+                        gradient = gradient_temp;
+                        
+                        % get new control with saturation
+                         for k = 1 : length(u_temp)
+                            value = u_temp(k) + step*direction(k);
+                            if value > ub
+                             u_temp(k) = ub;
+                            elseif value < lb
+                            u_temp(k) = lb;
+                            else
+                            u_temp(k) = value;
+                            end
+                         end
+                         % calculate quality indicator in this point
+                        [t,x,psi,gradient_temp] = rk4(@rhs,@rhs_sprzezone,x0,time,sample_time,Theta_0_ht,m,u_temp,nodes);
+                        Q_temp = x(end,end); 
+                        step = step * expansion;
+                    end
+                else
+                    Q = Q_temp;
+                    Reneval = 0;
+                    s = u_temp - u;
+                    r = gradient_temp - gradient;
+                    u = u_temp;
+                    gradient = gradient_temp;
+                end
+                
                 break;
             else
                 if step > 1e-15
+                    i
                     step = step * contraction;
                 else
                     Reneval = 1;
